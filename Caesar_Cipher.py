25f22adbef676a0cf31564a53e95e6ab05b4ac42
AleksHolland84 <43878016+AleksHolland84@users.noreply.github.com>
2021-02-02 13:19:18 +0100

    Create Caesar_Cipher.py

---
+++ Caesar_Cipher.py
@@ -0,0 +1,30 @@
+# uses the Caesar cipher to encrypt a text
+
+
+def caesar( plaintext, cipherkey ):
+
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ"
+    print(len(alphabet))
+    plaintext = plaintext.upper()
+    ciphertext = ""
+
+    # shift each letter in the plain text by the value of 'cipherkey'
+    for i in plaintext:
+        if i == " ":
+            chiphertext = ciphertext + " "
+        idx = alphabet.index(i)
+
+        # wrap around at top on encrypt
+        idx =  (idx + cipherkey) % len(alphabet) 
+
+        # prevent underflow on decrpt
+        if (idx<0): idx += len(alphabet)
+
+        # apply shift to the character        
+        ciphertext = ciphertext + alphabet[idx]
+
+    return(ciphertext)
+    
+    
+    if __name__=='__main__':
+        plaintext = input('Write your text: ')
