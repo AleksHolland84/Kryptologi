fe46d7e519a7fa580934785041aaf1f76c7f8704
AleksHolland84 <43878016+AleksHolland84@users.noreply.github.com>
2021-08-13 16:55:08 +0200

    Update Caecar_full.py

--- Caecar_full.py
+++ Caecar_full.py
@@ -51,6 +51,56 @@ def caesar_decrypt(ciphertext, cipherkey):
 
     return(plaintext)
  
+
+"""
+The hacker() functions are used to brute force the cipher text. 
+Since there there isn't a lot of "compinations" it can easly be
+brute forced.
+Though, the hacker() functions will break if the ciphered text doesn't 
+use a <space> as a string-delimiter to make a list of words.
+"""
+def hacker(ciphertext):
+    controller_text = "HEJ HALLO MED SKAL HISLEN test i"
+    for _ in range(0,29):
+        _plaintext = caesar_decrypt(ciphertext, _)
+        cipher_word = _plaintext.split(' ')
+        for word in cipher_word:
+            if len(word) > 2:
+                if word in controller_text.upper():
+                    print("Match found!")
+                    print(f'Decrypted with key: {_ }: {_plaintext}')
+                    ack = input('Is this correct? (y)es, (n)o: ')
+                    if ack == 'y':
+                        return _
+                    else:
+                        continue
+                else:
+                    pass 
+            else:
+                pass
+        print(f'Tried with key: {_} -   {_plaintext}')
+
+                
+def hacker2(ciphertext):
+    controller_text = "HEJ HALLO MED SKAL HISLEN test i"
+    for _ in range(0,29):
+        controller_encrypted =  caesar(controller_text, _)
+        cipher_word = controller_encrypted.split(' ')
+        
+        for word in cipher_word:
+            if len(word) > 2:
+                if word in ciphertext:
+                    print("Match found!")
+                    _plaintext = caesar_decrypt(ciphertext, _)
+                    print(f'Decrypted with key: {_ }: {_plaintext}')
+                    ack = input('Is this correct? (y)es, (n)o: ')
+                    if ack == 'y':
+                        return _
+                    else:
+                        continue
+                        
+
+# MAIN SCRIPT            
  
 if __name__=='__main__':
     while True:
